74c8e91435a00215b1cc0e269050bbd3eff9a976
snn <sinanm89@gmail.com>
2018-01-23 17:42:00 -0500

    working with data on appd no sorting

--- background.js
+++ background.js
@@ -7,13 +7,55 @@ var url = window.location.host;
 // (function create_tables(value) {
   // value = !value.flip_estate_wiki;
   // chrome.storage.sync.set({"flip_estate_wiki": value});
-  var query = { active: true, currentWindow: true };
-  var sometab;
-  function callback(tabs) {
-    var currentTab = tabs[0]; // there will be only one in this array
-    console.log(currentTab.url); // also has properties like currentTab.id
-    chrome.tabs.executeScript(currentTab.id, {
-          file: 'onload.js'
-      });
+var query = { active: true, currentWindow: true };
+var sometab;
+function callback(tabs) {
+  var currentTab = tabs[0]; // there will be only one in this array
+  console.log(currentTab.url); // also has properties like currentTab.id
+  chrome.tabs.executeScript(currentTab.id, {
+        file: 'onload.js'
+  }, function(tabs){
+    
+    chrome.storage.local.get({'page_data': {} }, function(page_data) {
+    console.log('READING FROM STORAGE');
+    page_data = page_data.page_data;
+    var elements = page_data.elements;
+    var col_size = page_data.column_size;
+    console.log(col_size, elements);
+    var tbl = document.createElement('table');
+    var table_div = document.getElementById('custom_table');
+    tbl.style.width = 'auto';
+    // tbl.style.height = 'auto';
+    tbl.setAttribute('border', '1');
+    var tbdy = document.createElement('tbody');
+
+    for (var i = 0, elem_i = 0; i < elements.length/col_size; i++) {
+        var tr = document.createElement('tr');
+        for (let j = 0; j < col_size; j++) {
+          var td = document.createElement('td');
+          // if ( typeof(elements[elem_i]) === 'undefined') { let element = " "; }
+          // else { let element = elements[elem_i]; }
+
+          td.appendChild(document.createTextNode(elements[elem_i]));
+          tr.appendChild(td)
+          elem_i++;
+            // } else {
+            // }
+                // i == 1 && j == 1 ? td.setAttribute('rowSpan', '2') : null;
+            // tr.appendChild(td)
+            
+        }
+        tbdy.appendChild(tr);
     }
-  chrome.tabs.query(query, callback);
+    // document
+    console.log('end stuff ----');
+    tbl.appendChild(tbdy);
+    table_div.appendChild(tbl);
+
+
+    table_div.style.width = '100%';
+    table_div.style.height = '100%';
+  });
+});
+}
+chrome.tabs.query(query, callback);
--- onload.js
+++ onload.js
@@ -4,25 +4,41 @@ console.log('I AM TRYING TO SAVE THE TABLES ON THE PAGE');
 // Get a value
 document.getElementsByClassName('ng-scope selected adsDashTreeButton')[0].click();
 // get key columns
-var column_key_names = document.querySelectorAll('[id^=gridcolumn-] span');
+
+// document.querySelectorAll('tbody [id^=treeview-]')[0].querySelectorAll("td")[6].textContent
+
+var column_key_names = document.querySelectorAll("[id^=gridcolumn-] span.x-column-header-text");
+
 var output = ['Name'];
+var key_names = [];
+
 // get values of each column
-for(let j=0; j<column_key_names.length-1; j++){
-  output.push(column_key_names[j].innerText);
+
+for(let j=0; j<column_key_names.length; j++){
+  output.push(column_key_names[j].textContent);
+}
+
+// var column_values = document.querySelectorAll('[id^=treeview-] tbody tr td div span:not(.x-tree-node-text)');
+
+var column_values = document.querySelectorAll('tbody [id^=treeview-]');
+
+for(var i=0; i<column_values.length+1; i++){
+    // if(i !== 0 && (i%column_key_names.length+1) !== 0){
+      for(let j=0; j<column_values[i].querySelectorAll("td").length+1; j++){        
+
+        let val = column_values[i].querySelectorAll("td")[j] ? column_values[i].querySelectorAll("td")[j].textContent : "";
+        output.push(val);
+    }
 }
 console.log(output);
-var column_values = document.querySelectorAll('[id^=treeview-] tbody tr td');
-// seek out children, go deep.
-for(let i=0; i<column_values.length-1; i++){
-    if(i !== 0 && (i%column_key_names.length) !== 0){
-      output.push(column_values[i].innerText);
-    // } else {
-      // output.append('\n')
-    }
-    
-}
+var results = {
+  'page_data':{
+    'elements': output,
+    'column_size': column_key_names.length + 1
+}};
 // Save it using the Chrome extension storage API.
-chrome.storage.local.set({'page_data': output}, function() {
+chrome.storage.local.set(results, function() {
   // Notify that we saved.
   console.log('Settings saved');
+
 });
